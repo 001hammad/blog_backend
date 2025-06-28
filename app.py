99e327628f3007b48439a4312a2f94e3617faee6
001hammad <hammadhafeez435@gmail.com>
2025-06-28 15:22:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -378,9 +378,7 @@ def get_all_comments():
 # --------------------------------------------
 # INIT
 # --------------------------------------------
-
-with app.app_context():
-    db.create_all()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)  # optional, for local dev only
