621df746cf531ee7d2faee9c8195360eeacbfb2d
001hammad <hammadhafeez435@gmail.com>
2025-06-29 19:05:29 +0500

    set mysql to postgresql

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 # SQLite DB config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:yueQiOhadxmOPuigWhtyanUKsbJDIYCI@turntable.proxy.rlwy.net:23697/railway'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
